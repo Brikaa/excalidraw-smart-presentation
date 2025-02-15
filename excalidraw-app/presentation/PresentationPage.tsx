c6e5ebf93d36855d54413e5522b99552976b52de
Omar Brikaa <brikaaomar@gmail.com>
2025-02-15 19:58:44 +0200

    Frame -> presentation exploration

--- excalidraw-app/presentation/PresentationPage.tsx
+++ excalidraw-app/presentation/PresentationPage.tsx
@@ -2,6 +2,32 @@ import { useEffect, useMemo } from "react";
 import { importFromLocalStorage } from "../data/localStorage";
 import { Excalidraw } from "../../packages/excalidraw";
 import { CODES } from "../../packages/excalidraw/keys";
+import type {
+  ExcalidrawElement,
+  ExcalidrawFrameElement,
+} from "../../packages/excalidraw/element/types";
+
+export function PresentationScene(props: {
+  elements: ExcalidrawElement[];
+  frame: ExcalidrawFrameElement;
+}) {
+  const { elements, frame } = props;
+  const frameElements = useMemo(
+    () => elements.filter((e) => e.frameId === frame?.id),
+    [elements, frame?.id],
+  );
+  const positionedElements = frameElements.map((e) => ({
+    ...e,
+    x: e.x - frame.x,
+    y: e.y - frame.y,
+  }));
+  return (
+    <Excalidraw
+      initialData={{ elements: positionedElements }}
+      viewModeEnabled
+    />
+  );
+}
 
 export function PresentationPage(props: {
   setPresentation: (enabled: boolean) => unknown;
@@ -15,21 +41,19 @@ export function PresentationPage(props: {
         setPresentation(false);
       }
     });
+    const appMenu = document.getElementsByClassName("App-menu");
+    for (const el of appMenu) {
+      el.remove();
+    }
   }, [setPresentation]);
 
   // Get first frame
   const frame = useMemo(
-    () => elements.find((e) => e.type === "frame"),
+    () => elements.find((e): e is ExcalidrawFrameElement => e.type === "frame"),
     [elements],
-  );
-  const frameElements = useMemo(
-    () => elements.filter((e) => e.frameId === frame?.id),
-    [elements, frame?.id],
   );
   if (!frame) {
     return null;
   }
-  return (
-    <Excalidraw initialData={{ elements: frameElements }} viewModeEnabled />
-  );
+  return <PresentationScene elements={elements} frame={frame} />;
 }
